a299e883cd5328b11ed6ee4dc3830c8ab8767147
gervasio.ramos <gervasio.ramos@poli.digital>
2023-03-16 17:37:58 -0300

    update

---
+++ _app/immutable/chunks/2.b9f0b47d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.97156d24.js";export{t as component};
---
+++ _app/immutable/entry/_layout.js.99828779.js
@@ -0,0 +1,1 @@
+import{l as a,p as o}from"../chunks/_layout.4cbd2f24.js";export{a as load,o as prerender};
