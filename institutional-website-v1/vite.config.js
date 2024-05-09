9aef8e81472021a91354548e3844bd026aab091e
JamilServices <jamilservicos@gmail.com>
2024-05-09 10:57:31 -0300

    :art: Update deployment path to 'docs' for GitHub Pages deployment.

--- institutional-website-v1/vite.config.js
+++ institutional-website-v1/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     root: './src',
     build: {
         manifest: false,
-        outDir: '../dist',
+        outDir: '../docs',
     },
     css: {
         postcss: {
